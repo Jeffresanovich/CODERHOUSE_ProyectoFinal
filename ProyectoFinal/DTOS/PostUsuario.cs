23f499b6a638eebdcb060cf7abebbc0970e409b8
Jeffresanovich <jeffresanovich@gmail.com>
2022-08-17 15:33:54 -0300

    Post-Put Usuario-Producto

---
+++ ProyectoFinal/DTOS/PostUsuario.cs
@@ -0,0 +1,12 @@
+namespace ProyectoFinal.Model
+{
+    public class PutUsuario
+    {
+        public int Id { set; get; }
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public string NombreUsuario { set; get; }
+        public string Contraseña { set; get; }
+        public string Mail { set; get; }
+    }
+}
---
+++ ProyectoFinal/DTOS/PutUsuario.cs
@@ -0,0 +1,11 @@
+namespace ProyectoFinal.Model
+{
+    public class PostUsuario
+    {
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public string NombreUsuario { set; get; }
+        public string Contraseña { set; get; }
+        public string Mail { set; get; }
+    }
+}
