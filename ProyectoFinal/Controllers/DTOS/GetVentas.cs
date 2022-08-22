b36102d79dfd59fb1feeb2ffedd989878e8ad3ec
Jeffresanovich <jeffresanovich@gmail.com>
2022-08-22 20:53:36 -0300

    creacion de clase getVenta

---
+++ ProyectoFinal/Controllers/DTOS/GetVentas.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoFinal.Controllers.DTOS
+{
+    public class GetVentas
+    {
+        public int Id { set; get; }
+        public string Comentarios { set; get; }
+        public string Descripciones { set; get; }
+        public double Costo { set; get; }
+        public double PrecioVenta { set; get; }
+        public int Stock { set; get; }
+        public string NombreUsuario { set; get; }
+    }
+}
